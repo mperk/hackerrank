ed9062680b5e971a2c8dd2a9eb9e5e1763ef4b19
Mehmet Perk <perkmehmed@gmail.com>
2019-10-06 15:23:21 +0300

    sock merchant solution

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
 
-namespace hackerrank
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+class Solution {
+
+
+
+    static void Main (string[] args) {
+       
+        Console.WriteLine();
+        Console.ReadLine();
     }
 }
